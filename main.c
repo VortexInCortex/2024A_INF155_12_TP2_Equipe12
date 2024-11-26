89ff092bf0da09eb692281578864d122a545789b
AzizTheGazele552 <therobot.mehrez@gmail.com>
2024-11-26 09:31:24 -0500

    petits ajustements
    pull request soon

--- main.c
+++ main.c
@@ -1,7 +1,5 @@
 #include <stdbool.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 #include "utils.h"
 #include "paire.h"
--- paire.c
+++ paire.c
@@ -17,9 +17,9 @@
  * @details la structure d'une paire. Elle contient une expression et sa definition.
  */
 struct s_paire {
-    char *expression; //  L'expression dont on donne une definition.
-    size_t longueur; //  La longueur de l'expression en nombre de caracteres.
-    char *definition; //  La definition associee a l'expression.
+	char *expression; //  L'expression dont on donne une definition.
+	size_t longueur; //  La longueur de l'expression en nombre de caracteres.
+	char *definition; //  La definition associee a l'expression.
 };
 
 //  ----------------------------------
@@ -38,38 +38,33 @@ t_paire *t_paire_creer(const char *expression, const char *definition) {
 }
 
 void t_paire_liberer(t_paire *paire) {
+	free(paire->expression);
+	free(paire->definition);
 	free(paire);
-	free(paire -> expression);
-	free(paire -> definition);
-
 }
 
 void t_paire_afficher(const t_paire *paire) {
-
-	printf("%s :",paire -> expression);
-	printf("%s \n",paire -> definition);
+	printf("%s : %s \n", paire->expression, paire->definition);
 }
 
 const char *t_paire_get_expression(const t_paire *paire) {
-	return paire -> expression;
+	return paire->expression;
 }
 
 size_t t_paire_get_longueur(const t_paire *paire) {
-	return paire -> longueur;
+	return paire->longueur;
 }
 
 const char *t_paire_get_definition(const t_paire *paire) {
-	return paire -> definition;
+	return paire->definition;
 }
 
 bool t_paire_contient(const t_paire *paire, const char *question) {
 	bool trouve = true;
 
-	//size_t longueur_question = paire -> longueur;
+	const char *expression_trouve = strstr(question, t_paire_get_expression(paire));
 
-	char *expression_trouve = strstr(question , paire -> expression);
-
-	if( expression_trouve == NULL) //La fonction strstr retourne NULL si l'expression n'est pas trouver dans la question
+	if (expression_trouve == NULL) //La fonction strstr retourne NULL si l'expression n'est pas trouver dans la question
 	{
 		trouve = false;
 	}
@@ -78,47 +73,46 @@ bool t_paire_contient(const t_paire *paire, const char *question) {
 }
 
 void t_paire_ecrire_fichier(FILE *fichier, t_paire *paire) {
-
-	fprintf(fichier,"%s : %s", t_paire_get_expression(paire),t_paire_get_definition(paire));
+	fprintf(fichier, "%s : %s", t_paire_get_expression(paire), t_paire_get_definition(paire));
 }
 
 void t_paire_test() {
-    printf("------------------------ TEST PAIRE ------------------------\n");
+	printf("------------------------ TEST PAIRE ------------------------\n");
 
-    //  t_paire_creer
-    t_paire *paire1 = t_paire_creer("expression",
-                                    "definition.");
-    assert(strcmp(paire1->expression, "expression") == 0);
-    assert(paire1->longueur==10);
-    assert(strcmp(paire1->definition, "definition.") == 0);
+	//  t_paire_creer
+	t_paire *paire1 = t_paire_creer("expression",
+	                                "definition.");
+	assert(strcmp(paire1->expression, "expression") == 0);
+	assert(paire1->longueur==10);
+	assert(strcmp(paire1->definition, "definition.") == 0);
 
-    //  t_paire_afficher
-    t_paire_afficher(paire1);
-    printf("\n");
+	//  t_paire_afficher
+	t_paire_afficher(paire1);
+	printf("\n");
 
-    //  t_paire_get_expression
-    assert(strcmp(t_paire_get_expression(paire1), "expression") == 0);
+	//  t_paire_get_expression
+	assert(strcmp(t_paire_get_expression(paire1), "expression") == 0);
 
-    //  t_paire_get_longueur
-    assert(t_paire_get_longueur(paire1) == 10);
+	//  t_paire_get_longueur
+	assert(t_paire_get_longueur(paire1) == 10);
 
-    //  t_paire_get_definition
-    assert(strcmp(t_paire_get_definition(paire1), "definition.") == 0);
+	//  t_paire_get_definition
+	assert(strcmp(t_paire_get_definition(paire1), "definition.") == 0);
 
-    t_paire *paire2 = t_paire_creer("protocole de communication",
-                                    "Specification de plusieurs regles pour un type de communication particulier");
+	t_paire *paire2 = t_paire_creer("protocole de communication",
+	                                "Specification de plusieurs regles pour un type de communication particulier");
 
-    //  t_paire_contient
-    assert(!t_paire_contient(paire1, "test"));
-    assert(t_paire_contient(paire1, "expression"));
-    assert(!t_paire_contient(paire2, "A quoi sert un bit en informatique ?"));
-    assert(t_paire_contient(paire2, "Qu'est-ce qu'un protocole de communication ?"));
+	//  t_paire_contient
+	assert(!t_paire_contient(paire1, "test"));
+	assert(t_paire_contient(paire1, "expression"));
+	assert(!t_paire_contient(paire2, "A quoi sert un bit en informatique ?"));
+	assert(t_paire_contient(paire2, "Qu'est-ce qu'un protocole de communication ?"));
 
-    //  t_paire_liberer
-    t_paire_liberer(paire1);
-    t_paire_liberer(paire2);
+	//  t_paire_liberer
+	t_paire_liberer(paire1);
+	t_paire_liberer(paire2);
 
-    assert(mon_rapport(false) == 0);
+	assert(mon_rapport(false) == 0);
 
-    printf("TEST paire : OK\n");
+	printf("TEST paire : OK\n");
 }
